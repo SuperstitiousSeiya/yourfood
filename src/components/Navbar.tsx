0b41ff5524044719a24418a42f2b06f63fdc474e
SuperstitiousSeiya <reyver.abellanosa@student.passerellesnumeriques.org>
2024-05-28 21:30:05 +0800

    fix searchbar navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,6 +58,7 @@ export default function Component() {
             className="pl-10 pr-4 py-2 rounded-md bg-gray-100 focus:bg-white focus:ring-1 focus:ring-orange-500 dark:bg-gray-800 dark:text-gray-50 dark:focus:bg-gray-700 w-full"
             placeholder="Search for dishes..."
             type="search"
+            value={query}
             onChange={(e) => setquery(e.target.value)}
           />
         </form>
